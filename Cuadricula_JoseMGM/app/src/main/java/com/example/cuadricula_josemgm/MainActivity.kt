50cb3c65ea268417dff924ad7a3419017f5f6a6a
Martin <josemartingarciamartinez237@gmail.com>
2023-09-20 19:55:40 -0600

    Crear cuadrícula de temas de cursos, así como asignarle espaciado y padding al contenido de la misma (Versión final)

--- Cuadricula_JoseMGM/app/src/main/java/com/example/cuadricula_josemgm/MainActivity.kt
+++ Cuadricula_JoseMGM/app/src/main/java/com/example/cuadricula_josemgm/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -23,6 +28,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.cuadricula_josemgm.data.DataSource
 import com.example.cuadricula_josemgm.model.Topic
 import com.example.cuadricula_josemgm.ui.theme.Cuadrícula_JoseMGMTheme
 
@@ -36,9 +42,22 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    TopicCardGrid()
                 }
             }
+        }
+    }
+}
+@Composable
+fun TopicCardGrid(){
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        contentPadding = PaddingValues(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ){
+        items(DataSource.topics){ topicCard->
+            TopicCard(topicCard)
         }
     }
 }
@@ -86,6 +105,6 @@ fun TopicCard(topic: Topic, modifier: Modifier=Modifier) {
 @Composable
 fun TopicPreview() {
     Cuadrícula_JoseMGMTheme {
-        TopicCard(Topic(R.string.photography, 321, R.drawable.photography))
+       TopicCardGrid()
     }
 }
